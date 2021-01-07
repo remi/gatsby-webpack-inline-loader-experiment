54305a6498a4135c8f8f980a866f738bb011c6ce
Rémi Prévost <remi@exomel.com>
2021-01-07 13:26:21 -0500

    Remove lots of stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Icon from "../images/github.png"
+import StaticIcon from "file-loader!../images/github.png"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <>
+    <p>Image in base64:</p>
+    <img src={Icon} width="32" alt="" />
+    <p>Image as an URL:</p>
+    <img src={StaticIcon} width="32" alt="" />
+  </>
 )
 
 export default IndexPage
